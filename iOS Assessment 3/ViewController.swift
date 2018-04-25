cd660fd703c8714412487cb8e7b5e1bc8cc2e0e4
BSASEF01 <bersabehsolomon16@gmail.com>
2018-04-25 14:39:17 -0400

    Initial Commit

---
+++ iOS Assessment 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Assessment 3
+//
+//  Created by Bersabeh Asefa on 4/25/18.
+//  Copyright © 2018 Bersabeh Asefa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
